9446689aef72abd4104c7da2af720783244eb6bb
Aileneboro <a.borovikova89@gmail.com>
2024-05-10 15:57:22 +0300

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+let mixedType: string | number;
+mixedType = "hello";
+mixedType = 10;
+
+type EnableOrDisable = "enable" | "disable";
+let value: EnableOrDisable;
+value = "enable";
+value = "disable";
